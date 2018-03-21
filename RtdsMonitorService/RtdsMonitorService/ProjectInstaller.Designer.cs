4bc244c584b68c962d0347cd8f2e12832564b87a
Brian Levy <b.levy@engusa.com>
2018-03-21 15:17:50 -0500

    Fixes more spacing issues and unnecessary using statements

--- RtdsMonitorService/RtdsMonitorService/ProjectInstaller.Designer.cs
+++ RtdsMonitorService/RtdsMonitorService/ProjectInstaller.Designer.cs
@@ -1,61 +1,61 @@
 ﻿namespace RtdsMonitorService
 {
-    partial class ProjectInstaller
+  partial class ProjectInstaller
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
 
-        /// <summary> 
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+    #region Component Designer generated code
 
-        #region Component Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.LmsMonitorServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
-            this.LmsIntegrationServiceInstaller = new System.ServiceProcess.ServiceInstaller();
-            // 
-            // LmsMonitorServiceProcessInstaller
-            // 
-            this.LmsMonitorServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
-            this.LmsMonitorServiceProcessInstaller.Password = null;
-            this.LmsMonitorServiceProcessInstaller.Username = null;
-            // 
-            // LmsIntegrationServiceInstaller
-            // 
-            this.LmsIntegrationServiceInstaller.Description = "Calls BREAD for OEE machines to find current status and reports any change in sta" +
-    "tus to be picked up by Preactor.";
-            this.LmsIntegrationServiceInstaller.DisplayName = "LmsIntegrationService";
-            this.LmsIntegrationServiceInstaller.ServiceName = "LmsIntegrationService";
-            this.LmsIntegrationServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
-            // 
-            // ProjectInstaller
-            // 
-            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.LmsMonitorServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+      this.LmsIntegrationServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+      // 
+      // LmsMonitorServiceProcessInstaller
+      // 
+      this.LmsMonitorServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+      this.LmsMonitorServiceProcessInstaller.Password = null;
+      this.LmsMonitorServiceProcessInstaller.Username = null;
+      // 
+      // LmsIntegrationServiceInstaller
+      // 
+      this.LmsIntegrationServiceInstaller.Description = "Calls BREAD for OEE machines to find current status and reports any change in sta" +
+"tus to be picked up by Preactor.";
+      this.LmsIntegrationServiceInstaller.DisplayName = "LmsIntegrationService";
+      this.LmsIntegrationServiceInstaller.ServiceName = "LmsIntegrationService";
+      this.LmsIntegrationServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+      // 
+      // ProjectInstaller
+      // 
+      this.Installers.AddRange(new System.Configuration.Install.Installer[] {
             this.LmsMonitorServiceProcessInstaller,
             this.LmsIntegrationServiceInstaller});
 
-        }
-
-        #endregion
-
-        private System.ServiceProcess.ServiceProcessInstaller LmsMonitorServiceProcessInstaller;
-        private System.ServiceProcess.ServiceInstaller LmsIntegrationServiceInstaller;
     }
+
+    #endregion
+
+    private System.ServiceProcess.ServiceProcessInstaller LmsMonitorServiceProcessInstaller;
+    private System.ServiceProcess.ServiceInstaller LmsIntegrationServiceInstaller;
+  }
 }
